5daf15b1d5d40af5cdaf0cd43faa88f456115562
Alexander Kelman <alexanderkelman95@hotmail.com>
2020-03-15 17:53:17 +0100

    Added DiscordBot.cs

---
+++ DiscordClient/DiscordBot.cs
@@ -0,0 +1,32 @@
+﻿using Discord.WebSocket;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiscordClient
+{
+    public class DiscordBot
+    {
+        private readonly DiscordSocketClient _client;
+        private readonly IConfiguration _config;
+
+        public DiscordBot()
+        {
+            _client = new DiscordSocketClient();
+        }
+
+        public async Task IntiateBot(string token)
+        {
+            try
+            {
+
+            }
+            catch (Exception ex)
+            {
+                throw new Exception($"Failed to initiate bot: {ex.Message}", ex.InnerException);
+            }
+        }
+    }
+}
