b251dd63cc4da6289c089fcd89053c948d8276b1
Alexander Kelman <alexanderkelman95@hotmail.com>
2020-03-15 18:06:22 +0100

    InitiateBot function

--- DiscordClient/Services/CommandHandler.cs
+++ DiscordClient/Services/CommandHandler.cs
@@ -1,10 +1,17 @@
-﻿using System;
+﻿using Discord.Commands;
+using Discord.WebSocket;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace DiscordClient.Services
 {
-    class CommandHandler
+    public class CommandHandler
     {
+        private readonly CommandService _commands;
+        private readonly DiscordSocketClient _client;
+        private readonly IServiceProvider _services;
+
+
     }
 }
