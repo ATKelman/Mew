280552bea1417207e5576fd99590ddf9a78a5c5a
Alexander Kelman <alexanderkelman95@hotmail.com>
2020-03-15 18:29:26 +0100

    DiscordBot and CommandHanlder setup

--- DiscordClient/DiscordBot.cs
+++ DiscordClient/DiscordBot.cs
@@ -1,6 +1,8 @@
 ﻿using Discord;
 using Discord.WebSocket;
+using DiscordClient.Services;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -11,10 +13,13 @@ namespace DiscordClient
     public class DiscordBot
     {
         private readonly DiscordSocketClient _client;
+        private readonly IServiceProvider _services;
 
-        public DiscordBot()
+        public DiscordBot(IServiceProvider services)
         {
             _client = new DiscordSocketClient();
+
+            _services = services;
         }
 
         public async Task IntiateBot(string token)
@@ -22,6 +27,13 @@ namespace DiscordClient
             try
             {
                 await _client.LoginAsync(TokenType.Bot, token);
+                await _client.StartAsync();
+
+                // Start command handler 
+                await _services.GetRequiredService<CommandHandler>().InitalizeAsync();
+
+                // Keep the program running until closed.
+                await Task.Delay(-1);
             }
             catch (Exception ex)
             {
--- DiscordClient/Services/CommandHandler.cs
+++ DiscordClient/Services/CommandHandler.cs
@@ -1,8 +1,12 @@
-﻿using Discord.Commands;
+﻿using Discord;
+using Discord.Commands;
 using Discord.WebSocket;
+using Microsoft.Extensions.DependencyInjection;
 using System;
 using System.Collections.Generic;
+using System.Reflection;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace DiscordClient.Services
 {
@@ -12,6 +16,44 @@ namespace DiscordClient.Services
         private readonly DiscordSocketClient _client;
         private readonly IServiceProvider _services;
 
+        public CommandHandler(IServiceProvider services)
+        {
+            _commands = services.GetRequiredService<CommandService>();
+            _client = services.GetRequiredService<DiscordSocketClient>();
+            _services = services;
 
+            _client.MessageReceived += MessageReceivedAsync;
+        }
+
+        public async Task InitalizeAsync()
+        {
+            // register modules - public & inherit ModuleBase
+            _ = await _commands.AddModulesAsync(Assembly.GetEntryAssembly(), _services);
+        }
+
+        private async Task MessageReceivedAsync(SocketMessage e)
+        {
+            try
+            {
+                SocketUserMessage msg = (SocketUserMessage)e;
+                if (msg == null) return;
+
+                CommandContext context = new CommandContext(_client, msg);
+                if (context.User.IsBot) return;
+
+                int argPos = 0;
+                if (msg.HasCharPrefix('!', ref argPos))
+                {
+                    var result = await _commands.ExecuteAsync(context, argPos, null);
+
+                    if (!result.IsSuccess)
+                        await context.Channel.SendMessageAsync(result.ErrorReason.ToString());
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception($"Failed to handle message: {ex.Message}", ex.InnerException);
+            }
+        }
     }
 }
