9d8454080c87946ceaf864ac1353ea5db8a5cf41
Alexander Kelman <alexanderkelman95@hotmail.com>
2020-03-31 20:51:10 +0200

    April Fools quick implementation

--- DiscordClient/Services/CommandHandlingService.cs
+++ DiscordClient/Services/CommandHandlingService.cs
@@ -2,6 +2,7 @@
 using Discord.WebSocket;
 using Microsoft.Extensions.DependencyInjection;
 using System;
+using System.Linq;
 using System.Reflection;
 using System.Threading.Tasks;
 
@@ -32,6 +33,8 @@ namespace DiscordClient.Services
                 CommandContext context = new CommandContext(_client, msg);
                 if (context.User.IsBot) return;
 
+                await AprilFools(msg, context);
+
                 int argPos = 0;
                 if (msg.HasCharPrefix('!', ref argPos))
                 {
@@ -45,6 +48,40 @@ namespace DiscordClient.Services
             {
                 throw new Exception($"Failed to handle message: {ex.Message}", ex.InnerException);
             }
+        }
+
+        private async Task AprilFools(SocketUserMessage msg, CommandContext context)
+        {
+            var channels = await context.Guild.GetChannelsAsync();
+            var random = new Random(DateTime.Now.Millisecond);
+
+            Discord.IGuildChannel channel = channels.ElementAt(0);
+            var channelFound = false;
+            while (!channelFound)
+            {
+                var rand = random.Next(0, channels.Count);
+                channel = channels.ElementAt(rand);
+                if (channel.Name.Equals("NIPPON") || channel.Name.Equals("nippon-lesson") || channel.Name.Equals("Nippon Voice") 
+                    || channel.Name.Equals("General") || channel.Name.Equals("Gaming 1") || channel.Name.Equals("Gaming 2") || channel.Name.Equals("nsfw-thestoryofbotkun") 
+                    || channel.Name.Equals("test1") || channel.Name.Equals("VOICE CHANNELS"))
+                {
+                    continue;
+                }
+
+                if (channel == context.Channel)
+                {
+                    continue;
+                }
+
+                channelFound = true;
+            }
+
+            // Delete message 
+            await msg.DeleteAsync();
+
+            var newMessage = $"{context.User.Mention} : {msg.Content}";
+            var ch = _client.GetChannel(channel.Id) as ISocketMessageChannel;
+            await ch.SendMessageAsync(newMessage);
         }
     }
 }
